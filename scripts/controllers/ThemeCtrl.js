d259bac4f240a33fef032ecd3767af3d3acad978
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-01-01 22:04:23 +0200

    modify var function added

--- scripts/controllers/ThemeCtrl.js
+++ scripts/controllers/ThemeCtrl.js
@@ -1,3 +1,7 @@
 function ThemeCtrl($scope, $routeParams){
     $scope.theme = $routeParams.themeName;
+
+    $scope.modifyLessVar = function(key, val){
+        less.modifyVars({ key: val });
+    }
 }
